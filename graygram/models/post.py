73b6abc31e6bb7e9ede412ab6d9ab5043a428a73
Suyeol Jeon <devxoul@gmail.com>
2017-02-17 04:28:27 +0900

    Add like related hybrid properties/methods to model Post

--- graygram/models/post.py
+++ graygram/models/post.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from sqlalchemy.ext.hybrid import hybrid_method
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.sql import functions as sqlfuncs
 
+from graygram import m
 from graygram.orm import db
 
 
@@ -18,6 +21,48 @@ class Post(db.Model):
 
     created_at = db.Column(db.DateTime(timezone=True), nullable=False,
                            server_default=sqlfuncs.now())
+
+    @hybrid_method
+    def is_liked_by(self, user):
+        post_like = m.PostLike.query \
+            .filter_by(user_id=user.id, post_id=self.id) \
+            .first()
+        return post_like is not None
+
+    @is_liked_by.expression
+    def is_liked_by(cls, user):
+        return m.PostLike.query \
+            .filter_by(user_id=user.id, post_id=cls.id) \
+            .exists()
+
+    @hybrid_property
+    def is_liked(self):
+        from flask_login import current_user
+        if not current_user.is_authenticated:
+            return False
+        return self.is_liked_by(current_user)
+
+    @is_liked.expression
+    def is_liked(cls):
+        from flask_login import current_user
+        if not current_user.is_authenticated:
+            return False
+        return cls.is_liked_by(current_user)
+
+    @is_liked.setter
+    def is_liked(self, value):
+        from flask_login import current_user
+        if not current_user.is_authenticated:
+            return
+        if value:
+            post_like = m.PostLike(user_id=current_user.id, post_id=self.id)
+            db.session.add(post_like)
+        else:
+            post_like = m.PostLike.query \
+                .filter_by(user_id=current_user.id, post_id=self.id) \
+                .first()
+            if post_like:
+                db.session.delete(post_like)
 
     def serialize(self):
         return {
