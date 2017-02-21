6aa524e163b11193d4e74bbb68f9843bfb7deb31
Suyeol Jeon <devxoul@gmail.com>
2017-02-22 02:27:53 +0900

    Add sphinxcontrib.autohttp.flask as an extension for sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,10 @@ import sphinx_rtd_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.httpdomain']
+extensions = [
+    'sphinxcontrib.httpdomain',
+    'sphinxcontrib.autohttp.flask',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
